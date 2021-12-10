c0075424ea55d961dcf8ee3ae0d21521bd7ec3f1
Qingwei Lan <qingweilan@gmail.com>
2021-12-09 21:01:24 -0600

    [model] update

--- src/strategy.py
+++ src/strategy.py
@@ -2,15 +2,15 @@ import pandas as pd
 import ta
 
 
-def bh(data: pd.DataFrame) -> pd.DataFrame:
+def bh(data: pd.DataFrame, start: int = -1402, end: int = -1) -> pd.DataFrame:
     bh_data = data.copy(deep=True)
     bh_data['action'] = bh_data.apply(lambda _: 'H', axis=1)
-    bh_data = bh_data[-1402:-1]
+    bh_data = bh_data[start:end]
     bh_data.loc[bh_data.index[0], 'action'] = 'B'
     return bh_data
 
 
-def macd(data: pd.DataFrame) -> pd.DataFrame:
+def macd(data: pd.DataFrame, start: int = -1402, end: int = -1) -> pd.DataFrame:
     def macd_cross(row):
         if row['trend_macd_diff_shifted'] > 0 and row['trend_macd_diff'] < 0:
             return 'B'
@@ -25,5 +25,5 @@ def macd(data: pd.DataFrame) -> pd.DataFrame:
     macd_data['trend_macd_diff_shifted'] = macd_data['trend_macd_diff'].shift(-1)
     macd_data['cross'] = macd_data.apply(lambda row: macd_cross(row), axis=1)
     macd_data['action'] = macd_data['cross'].shift(1)
-    macd_data = macd_data[-1402:-1][["Open", "High", "Low", "Close", "Volume", "action"]]
+    macd_data = macd_data[start:end][["Open", "High", "Low", "Close", "Volume", "action"]]
     return macd_data
